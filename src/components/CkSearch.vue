a7a516be7259793fa5cd42b018815aaf490a67d0
kkouken <410913568@qq.com>
2016-10-21 15:52:46 +0800

    update && save code

---
+++ src/components/CkSearch.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<div class="ck-search">
+			<form action='' method="GET">
+				<div class="input-group input-group-lg">
+					<span class="input-group-addon ck-search-select-wrap">
+						<select class="ck-search-select">
+							<option value="p">商品</option>
+							<option value="s">店铺</option>
+						</select>
+					</span>
+					<input type="search" name="query" class="form-control" placeholder="搜索关键字..."/>
+					<span class="input-group-btn">
+						<button type="submit" class="btn btn-primary">
+							搜索
+						</button>
+					</span>
+				</div>
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import CkSearch from 'components/CkSearch'
+
+export default {
+	components:{
+		CkSearch
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+@import '../assets/css/icons.css';
+@import '../assets/css/bootstrap.css';
+@import "../assets/less/CkSearch.less";
+@import "../assets/less/public.less";
+@import "../assets/less/C.less";
+</style>
