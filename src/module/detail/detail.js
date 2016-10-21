decbbd316ea7fcb8388de625939994d2d78026c5
Liang Cheng <1101126974@qq.com>
2016-10-21 16:11:23 +0800

    all

--- src/module/detail/detail.js
+++ src/module/detail/detail.js
@@ -3,6 +3,6 @@ import detail from './detail.vue'
 
 /* eslint-disable no-new */
 new Vue({
-  el: 'body',
+  el: '#detail',
   render: h => h(detail)
 })
---
+++ src/module/seller_register/seller_register.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import seller_register from './seller_register.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#seller_register',
+  render: h => h(seller_register)
+})
