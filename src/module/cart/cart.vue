381cef6c4b9c285875b111b617be5615b860cc62
Liang Cheng <1101126974@qq.com>
2016-10-25 13:37:50 +0800

    all

---
+++ src/module/cart/cart.vue
@@ -0,0 +1,200 @@
+<template>
+  <div>
+    <div class="cart-m">
+      <div class="container">      
+          <div class="row">
+            <div class="col-md-12">
+              <div class="cart">
+                <ul class="cart-menu">
+                  <li class="menu-all">
+                    <input type="checkbox">
+                    <label>全选</label>
+                  </li>
+                  <li class="menu-shp">商品</li>
+                  <li class="menu-sty">规格</li>
+                  <li class="menu-num">数量</li>
+                  <li class="menu-pri">单价</li>
+                  <li class="menu-tot">小计</li>
+                  <li class="menu-act">操作</li>
+                </ul>
+
+                <div class="cart-detail">
+                  <div class="detail-tit">
+                    <input type="checkbox">
+                    <div class="shp-logo">SHOP</div>
+                    <div>ABCDE</div>
+                    <span class="icon-dianhua"></span>
+                    <div>12345612345</div>
+                  </div>
+                  <div class="detail-shp">
+                    <div class="detail-list">
+                      <div class="list-shp">
+                        <input type="checkbox">
+                        <img src="../../assets/images/cart-show.jpg" height="80" width="80">
+                        <div>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</div>
+                      </div>
+                      <div class="list-sty">
+                        黑色/XL码
+                      </div>
+                      <div class="list-num">
+                        <div class="num-box">
+                          <button dir="sub">-</button>
+                          <input type="text">
+                          <button dir="add">+</button>
+                        </div>
+                        <div class="num-text">
+                          6-15件 : 110/件
+                        </div>
+                      </div>
+                      <div class="list-pri">
+                        &yen; 110.00
+                      </div>
+                      <div class="list-tot">
+                        &yen; 1255.00
+                      </div>
+                      <div class="list-act">
+                        <a>删除</a> | <a>收藏</a>
+                      </div>
+                    </div>
+                    <div class="detail-list">
+                      <div class="list-shp">
+                        <input type="checkbox">
+                        <img src="../../assets/images/cart-show.jpg" height="80" width="80">
+                        <div>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</div>
+                      </div>
+                      <div class="list-sty">
+                        黑色/XL码
+                      </div>
+                      <div class="list-num">
+                        <div class="num-box">
+                          <button dir="sub">-</button>
+                          <input type="text">
+                          <button dir="add">+</button>
+                        </div>
+                        <div class="num-text">
+                          6-15件 : 110/件
+                        </div>
+                      </div>
+                      <div class="list-pri">
+                        &yen; 110.00
+                      </div>
+                      <div class="list-tot">
+                        &yen; 1255.00
+                      </div>
+                      <div class="list-act">
+                        <a>删除</a> | <a>收藏</a>
+                      </div>
+                    </div>
+                    <div class="detail-send">
+                      <div>配送服务 : </div>
+                      <div>
+                        <input type="checkbox">
+                        <label>代发</label>
+                      </div>
+                      <div>
+                        <input type="checkbox">
+                        <label>商家发货</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="cart-detail">
+                  <div class="detail-tit">
+                    <input type="checkbox">
+                    <div class="shp-logo">SHOP</div>
+                    <div>ABCDE</div>
+                    <span class="icon-dianhua"></span>
+                    <div>12345612345</div>
+                  </div>
+                  <div class="detail-shp">
+                    <div class="detail-list">
+                      <div class="list-shp">
+                        <input type="checkbox">
+                        <img src="../../assets/images/cart-show.jpg" height="80" width="80">
+                        <div>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</div>
+                      </div>
+                      <div class="list-sty">
+                        黑色/XL码
+                      </div>
+                      <div class="list-num">
+                        <div class="num-box">
+                          <button dir="sub">-</button>
+                          <input type="text">
+                          <button dir="add">+</button>
+                        </div>
+                        <div class="num-text">
+                          6-15件 : 110/件
+                        </div>
+                      </div>
+                      <div class="list-pri">
+                        &yen; 110.00
+                      </div>
+                      <div class="list-tot">
+                        &yen; 1255.00
+                      </div>
+                      <div class="list-act">
+                        <a>删除</a> | <a>收藏</a>
+                      </div>
+                    </div>
+                    <div class="detail-send">
+                      <div>配送服务 : </div>
+                      <div>
+                        <input type="checkbox">
+                        <label>代发</label>
+                      </div>
+                      <div>
+                        <input type="checkbox">
+                        <label>商家发货</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="cart-oth">
+                  <div class="oth-l">
+                    <input type="checkbox">
+                    <label>全选</label>
+                    <a>删除</a>
+                    <a>收藏</a>
+                    <a>清除失效商品</a>
+                  </div>
+                  <div class="oth-r">
+                    <span>商品数量 : 12件</span>
+                    <span>金额总计(不含运费) : <mark>&yen;1255.00</mark></span>
+                    <button>结算</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>  
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+  import Vue from 'vue'
+  const VueResource = require('vue-resource')
+  Vue.use(VueResource)
+  export default{
+    data () {
+      return {
+       
+      }
+    },
+    methods: {
+    
+    },
+    mounted () {
+      
+    }
+  }
+</script>
+<style lang="less">
+  @import "../../assets/less/cart.less";
+  @import '../../assets/css/icons.css';
+  @import '../../assets/css/bootstrap.css';
+</style>
--- src/module/register/register.vue
+++ src/module/register/register.vue
@@ -1,67 +1,87 @@
 <template>
 <div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 header">
-        <h2>我是头部</h2>
+  <div class="register-t">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="register-t-tit">
+            <img src="../../assets/images/logo.svg" width="150">
+            <span>欢迎注册</span>
+          </div>          
+        </div>
       </div>
-    </div>
+    </div>   
   </div>
   
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="register">
-          <div class="register-head">
-            <div class="register-head-tit">
-              新用户注册
+  <div class="register-m">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="register">
+            <div class="register-head">
+              <div class="register-head-tit">
+                新用户注册
+              </div>
+              <div class="register-head-menu">
+                <span>已有账号?</span>
+                <span><a href="#">立即登录</a></span>
+              </div>
             </div>
-            <div class="register-head-menu">
-              <span>已有账号?</span>
-              <span><a href="#">立即登录</a></span>
-            </div>
+            <form>
+              <div class="form-phone">
+                <label>手<span class="em"></span><span class="em"></span>机</label>
+                <div class="input-box">
+                  <span class="icon-zhanghao"></span>
+                  <input type="text" name="phone" placeholder="请输入手机号码">
+                </div>                        
+              </div>
+              <div class="form-psd">
+                <label>密<span class="em"></span><span class="em"></span>码</label>
+                <div class="input-box">
+                  <span class="icon-mima"></span>
+                  <input type="password" name="psd" placeholder="请输入密码">
+                </div>
+              </div>
+              <div class="form-psd-cfig">
+                <label>确认密码</label>
+                <div class="input-box">
+                  <span class="icon-mima"></span>
+                  <input type="password" name="psdcfig" placeholder="请确认密码">
+                </div>
+              </div>
+              <div class="form-verify">
+                <label>短信验证</label>
+                <div class="input-box">
+                  <input type="text" name="verify" placeholder="请输入验证码">
+                  <button type="button" v-bind:class="sendMsg ? 'after' : 'before'" v-bind:disabled="sendMsg" v-on:click="getCode">{{btnContent}}</button>
+                </div>            
+              </div>
+              <div class="form-xieyi">
+                <input type="checkbox">
+                <label>我已阅读并同意<a href="#">《柯咔网络用户服务协议》</a></label>
+              </div>
+              <div class="form-submit">
+                <button>注<span class="em"></span><span class="em"></span>册</button>
+              </div>
+            </form>
           </div>
-          <form>
-            <div class="form-phone">
-              <label>手<span class="em"></span><span class="em"></span>机</label>
-              <div class="input-box">
-                <span class="icon-zhanghao"></span>
-                <input type="text" name="phone" placeholder="请输入手机号码">
-              </div>                        
-            </div>
-            <div class="form-psd">
-              <label>密<span class="em"></span><span class="em"></span>码</label>
-              <div class="input-box">
-                <span class="icon-mima"></span>
-                <input type="password" name="psd" placeholder="请输入密码">
-              </div>
-            </div>
-            <div class="form-psd-cfig">
-              <label>确认密码</label>
-              <div class="input-box">
-                <span class="icon-mima"></span>
-                <input type="password" name="psdcfig" placeholder="请确认密码">
-              </div>
-            </div>
-            <div class="form-verify">
-              <label>短信验证</label>
-              <div class="input-box">
-                <input type="text" name="verify" placeholder="请输入验证码">
-                <button type="button" v-bind:class="sendMsg ? 'after' : 'before'" v-bind:disabled="sendMsg" v-on:click="getCode">{{btnContent}}</button>
-              </div>            
-            </div>
-            <div class="form-xieyi">
-              <input type="checkbox">
-              <label>我已阅读并同意<a href="#">《柯咔网络用户服务协议》</a></label>
-            </div>
-            <div class="form-submit">
-              <button>注<span class="em"></span><span class="em"></span>册</button>
-            </div>
-          </form>
         </div>
       </div>
     </div>
   </div>
+  
+
+  <div class="register-b">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="register-b-tit">
+            Copyright 2016 广州柯咔网络科技有限公司版权所有 粤ICP备XXXXXXXXX号 客服电话 : 400-020-xxxxxxx
+          </div>          
+        </div>
+      </div>
+    </div>   
+  </div>  
 </div>
 </template>
 
@@ -75,7 +95,7 @@
       return {
         btnContent: '获取短信验证码',
         sendMsg: false,
-        sendTime: 3
+        sendTime: 60
       }
     },
     methods: {
@@ -88,7 +108,7 @@
           me.btnContent = '获取验证码(' + me.sendTime + ')'
           if(me.sendTime == 0){
             clearInterval(timer)
-            me.sendTime = 3
+            me.sendTime = 60
             me.sendMsg = false
             me.btnContent = '获取短信验证码'
           }
