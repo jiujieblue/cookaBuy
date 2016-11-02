4ae9eb8caf97f8e530c47ea1a0e3172cf43d6e70
kkouken <410913568@qq.com>
2016-11-02 18:10:20 +0800

    save code

--- src/module/orderconfirm/orderdetail.vue
+++ src/module/orderconfirm/orderdetail.vue
@@ -19,20 +19,88 @@
 					<div class="shop-title">
 						<span>SHOP</span>
 						<span>嘟嘟服饰</span>
-						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107(<i class="icon-dianhua"></i>13760636639)</span>
+						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107&nbsp;(&nbsp;13760636639&nbsp;)</span>
 					</div>
 					<div class="shop-body">
 						<table class="shop_tb">
-						<tr>
-							<td>商品</td>
-							<td>规格</td>
-							<td>单价(元)</td>
-							<td>数量</td>
-							<td>商品总价(元)</td>
-							<td>代发服务费</td>
-							<td>小计</td>
-						</tr>
-					</table>
+							<tr>
+								<td>
+									<div class="img">
+										<img src="../../assets/images/test01.jpg" alt="">
+									</div>
+									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
+									<span>货号：8634</span>
+								</td>
+								<td>黑色/175L</td>
+								<td>18.00</td>
+								<td>5</td>
+								<td>90.00</td>
+								<td>10.00</td>
+								<td>100.00</td>
+							</tr>
+							<tr>
+								<td>
+									<div class="img">
+										<img src="../../assets/images/test01.jpg" alt="">
+									</div>
+									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
+									<span>货号：8634</span>
+								</td>
+								<td>黑色/175L</td>
+								<td>18.00</td>
+								<td>5</td>
+								<td>90.00</td>
+								<td>10.00</td>
+								<td>100.00</td>
+							</tr>
+						</table>
+					</div>
+					<div class="shop-footer">
+						<span>配送服务：代发</span>
+					</div>
+				</div>
+				<div class="shop">
+					<div class="shop-title">
+						<span>SHOP</span>
+						<span>liangcheng服饰</span>
+						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107&nbsp;(&nbsp;13760636639&nbsp;)</span>
+					</div>
+					<div class="shop-body">
+						<table class="shop_tb">
+							<tr>
+								<td>
+									<div class="img">
+										<img src="../../assets/images/test02.jpg" alt="">
+									</div>
+									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
+									<span>货号：8634</span>
+								</td>
+								<td>黑色/175L</td>
+								<td>18.00</td>
+								<td>5</td>
+								<td>90.00</td>
+								<td>10.00</td>
+								<td>100.00</td>
+							</tr>
+							<tr>
+								<td>
+									<div class="img">
+										<img src="../../assets/images/test02.jpg" alt="">
+									</div>
+									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
+									<span>货号：8634</span>
+								</td>
+								<td>黑色/175L</td>
+								<td>18.00</td>
+								<td>5</td>
+								<td>90.00</td>
+								<td>10.00</td>
+								<td>100.00</td>
+							</tr>
+						</table>
+					</div>
+					<div class="shop-footer">
+						<span>配送服务：代发</span>
 					</div>
 				</div>
 			</div>
