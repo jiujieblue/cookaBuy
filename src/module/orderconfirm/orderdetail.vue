c4c7100a0b9203f94fe873c9ea4f5bae7c2ca411
heyaopeng <he_yaopeng@163.com>
2016-11-10 17:50:47 +0800

    pull code

--- src/module/orderconfirm/orderdetail.vue
+++ src/module/orderconfirm/orderdetail.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="orderdetail-box">
 		<div class="title">订单详情</div>
-		<div class="daifa-box">
+		<div class="daifa-box" v-if="daifaList.length != 0">
 			<div class="daifa-box-top">代发</div>
 			<div class="daifa-box-body">
 				<table class="daifa_tb">
@@ -15,84 +15,25 @@
 						<td>小计</td>
 					</tr>
 				</table>
-				<div class="shop">
+				<div class="shop" v-for="(daifaItem, daifaIndex) in daifaList">
 					<div class="shop-title">
 						<span>SHOP</span>
-						<span>嘟嘟服饰</span>
-						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107&nbsp;(&nbsp;13760636639&nbsp;)</span>
+						<span>{{daifaItem.store_name}}</span>
+						<span>{{daifaItem.location}}&nbsp;({{daifaItem.mobile}})</span>
 					</div>
-					<div class="shop-body">
+					<div class="shop-body" >
 						<table class="shop_tb">
-							<tr>
+							<tr v-for="(daifaCart, daifaCartIndex) in daifaItem.cart">
 								<td>
 									<div class="img">
-										<img src="../../assets/images/test01.jpg" alt="">
+										<img :src="daifaCart.pic_url" alt="">
 									</div>
-									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
+									<span>{{daifaCart.title}}</span>
 									<span>货号：8634</span>
 								</td>
-								<td>黑色/175L</td>
-								<td>18.00</td>
-								<td>5</td>
-								<td>90.00</td>
-								<td>10.00</td>
-								<td>100.00</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="img">
-										<img src="../../assets/images/test01.jpg" alt="">
-									</div>
-									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
-									<span>货号：8634</span>
-								</td>
-								<td>黑色/175L</td>
-								<td>18.00</td>
-								<td>5</td>
-								<td>90.00</td>
-								<td>10.00</td>
-								<td>100.00</td>
-							</tr>
-						</table>
-					</div>
-					<div class="shop-footer">
-						<div>配送服务：代发</div>
-					</div>
-				</div>
-				<div class="shop">
-					<div class="shop-title">
-						<span>SHOP</span>
-						<span>liangcheng服饰</span>
-						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107&nbsp;(&nbsp;13760636639&nbsp;)</span>
-					</div>
-					<div class="shop-body">
-						<table class="shop_tb">
-							<tr>
-								<td>
-									<div class="img">
-										<img src="../../assets/images/test02.jpg" alt="">
-									</div>
-									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
-									<span>货号：8634</span>
-								</td>
-								<td>黑色/175L</td>
-								<td>18.00</td>
-								<td>5</td>
-								<td>90.00</td>
-								<td>10.00</td>
-								<td>100.00</td>
-							</tr>
-							<tr>
-								<td>
-									<div class="img">
-										<img src="../../assets/images/test02.jpg" alt="">
-									</div>
-									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
-									<span>货号：8634</span>
-								</td>
-								<td>黑色/175L</td>
-								<td>18.00</td>
-								<td>5</td>
+								<td>{{daifaCart.pro_name}}</td>
+								<td>{{daifaCart.origin_price}}</td>
+								<td>{{daifaCart.num}}</td>
 								<td>90.00</td>
 								<td>10.00</td>
 								<td>100.00</td>
@@ -132,7 +73,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="shangjiafahuo-box">
+		<div class="shangjiafahuo-box" v-if="fahuoList != 0">
 			<div class="shangjiafahuo-box-top">商家发货</div>
 			<div class="shangjiafahuo-box-body">
 				<table class="shangjiafahuo_tb">
@@ -146,133 +87,27 @@
 						<td>小计</td>
 					</tr>
 				</table>
-				<div class="order">
+				<div class="order" v-for="(fahuoItem, fahuoIndex) in fahuoList">
 					<div class="order-title">
 						<span>SHOP</span>
-						<span>bizhi服饰</span>
-						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107&nbsp;(&nbsp;13760636639&nbsp;)</span>
+						<span>{{fahuoItem.store_name}}</span>
+						<span>{{fahuoItem.location}}&nbsp;(&nbsp;{{fahuoItem.mobile}}&nbsp;)</span>
 					</div>
 					<div class="order-body">
 						<table class="order_tb">
-							<tr>
+							<tr v-for="(fahuoCart, fahuoCartIndex) in fahuoItem.cart">
 								<td>
 									<div class="img">
-										<img src="../../assets/images/test01.jpg" alt="">
+										<img :src="fahuoCart.pic_url" alt="">
 									</div>
-									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
+									<span>{{fahuoCart.title}}</span>
 									<span>货号：8634</span>
 								</td>
-								<td>黑色/175L</td>
-								<td>18.00</td>
-								<td>5</td>
+								<td>{{fahuoCart.pro_name}}</td>
+								<td>{{fahuoCart.origin_price}}</td>
+								<td>{{fahuoCart.num}}</td>
 								<td>90.00</td>
-								<td>10.00</td>
-								<td>100.00</td>
-							</tr>
-						</table>
-						<div class="peisong">
-							<div class="peisong-left">
-								<div class="way">
-									<span>配送方式：</span>
-									<select name="" id="">
-										<option value="">快递：¥7.00</option>
-										<option value="">平邮：¥7.00</option>
-										<option value="">EMS：¥7.00</option>
-									</select>
-								</div>
-								<div class="comment">
-									<span>订单留言：</span>
-									<textarea name="" id="" cols="70" rows="4" placeholder="最多输入100个字"></textarea>
-									<div>您的特殊要求需要跟商家确认一致，否则视为无效。</div>
-								</div>
-							</div>
-							<div class="peisong-right">
-								<div class="tip">
-									<div>运费：7.00元<i class="icon-wenhao"></i></div>
-									<div class="tip-modal">
-										<div class="square"></div>
-										<div class="triangle"></div>
-										<div class="content">运费费率：首重7.00元/公斤，续重3.00元/公斤</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="order">
-					<div class="order-title">
-						<span>SHOP</span>
-						<span>bizhi服饰</span>
-						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107&nbsp;(&nbsp;13760636639&nbsp;)</span>
-					</div>
-					<div class="order-body">
-						<table class="order_tb">
-							<tr>
-								<td>
-									<div class="img">
-										<img src="../../assets/images/test01.jpg" alt="">
-									</div>
-									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
-									<span>货号：8634</span>
-								</td>
-								<td>黑色/175L</td>
-								<td>18.00</td>
-								<td>5</td>
-								<td>90.00</td>
-								<td>10.00</td>
-								<td>100.00</td>
-							</tr>
-						</table>
-						<div class="peisong">
-							<div class="peisong-left">
-								<div class="way">
-									<span>配送方式：</span>
-									<select name="" id="">
-										<option value="">快递：¥7.00</option>
-										<option value="">平邮：¥7.00</option>
-										<option value="">EMS：¥7.00</option>
-									</select>
-								</div>
-								<div class="comment">
-									<span>订单留言：</span>
-									<textarea name="" id="" cols="70" rows="4" placeholder="最多输入100个字"></textarea>
-									<div>您的特殊要求需要跟商家确认一致，否则视为无效。</div>
-								</div>
-							</div>
-							<div class="peisong-right">
-								<div class="tip">
-									<div>运费：7.00元<i class="icon-wenhao"></i></div>
-									<div class="tip-modal">
-										<div class="square"></div>
-										<div class="triangle"></div>
-										<div class="content">运费费率：首重7.00元/公斤，续重3.00元/公斤</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="order">
-					<div class="order-title">
-						<span>SHOP</span>
-						<span>jinbiao服饰</span>
-						<span>广东省&nbsp;广州市&nbsp;沙河区&nbsp;新潮都-1F-107&nbsp;(&nbsp;13760636639&nbsp;)</span>
-					</div>
-					<div class="order-body">
-						<table class="order_tb">
-							<tr>
-								<td>
-									<div class="img">
-										<img src="../../assets/images/test01.jpg" alt="">
-									</div>
-									<span>2016年夏季圆领宽松腰贴布中长款短袖连衣裙</span>
-									<span>货号：8634</span>
-								</td>
-								<td>黑色/175L</td>
-								<td>18.00</td>
-								<td>5</td>
-								<td>90.00</td>
-								<td>10.00</td>
+								<td>————</td>
 								<td>100.00</td>
 							</tr>
 						</table>
@@ -316,7 +151,10 @@
 	export default{
 		data(){
 			return{
-        list:[]
+        list:[],
+        part:'',
+        daifaList: [],
+        fahuoList: []
 			}
 		},
 		components:{
@@ -328,6 +166,15 @@
 		mounted(){
       this.list = JSON.parse(localStorage.getItem('list'))
       console.log(this.list)
+      for(var i = 0 ; i < this.list.length ; i ++){
+        if(this.list[i].send==2) {
+          this.part = '商家发货'
+          this.fahuoList.push(this.list[i])
+        }
+        else{
+          this.daifaList.push(this.list[i])
+        }
+      }
 		}
 	}
 </script>
