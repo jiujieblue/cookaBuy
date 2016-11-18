3dee758c9f22807f9a93afb3054ecb3474079420
kkouken <410913568@qq.com>
2016-11-18 11:28:32 +0800

    save code

--- src/module/seller_childaccountmanage/seller_childaccountlist.vue
+++ src/module/seller_childaccountmanage/seller_childaccountlist.vue
@@ -17,10 +17,10 @@
 						</div>
 
 						<!-- 子账户列表 -->
-						<div class="cam-calist" v-if="false">
+						<div class="cam-calist" v-if="IsShowListDiv">
 							<div class="title">
 								<span>子账户列表</span>
-								<button class="btn">添加子账户</button>
+								<button class="btn" @click="_AddCA">添加子账户</button>
 							</div>
 							<div class="clearfix"></div>
 							<div class="listbody">
@@ -41,7 +41,7 @@
 										<td>启用</td>
 										<td>
 											<button class="btn-link" @click="_changeStatus">停用</button>
-											<button class="btn-link">修改</button>
+											<button class="btn-link" @click="_EditCA">修改</button>
 										</td>
 									</tr>
 									<tr>
@@ -93,135 +93,271 @@
 						</div>
 						
 						<!-- 添加 子账户 -->
-						<div class="cam-add_ac">
+						<div class="cam-add_ac" v-if="IsShowAddDiv">
 							<div class="title">添加子账号</div>
-							<div class="body">
-								<div class="left">
-									<div>
-										<span>手机号：</span>
-										<input type="text">
+							<form action="">
+								<div class="body">
+									<div class="left">
+										<div>
+											<span>手机号：</span>
+											<input type="text">
+										</div>
+										<div>
+											<span>姓<i class="em"></i>名：</span>
+											<input type="text">
+										</div>
+										<div>
+											<span>账<i class="em"></i>号：</span>
+											<input type="text">
+										</div>
+										<div class="dottedline"></div>
+										<div>
+											<span>密<i class="em"></i><i class="em"></i>码：</span>
+											<input type="text">
+										</div>
+										<div>
+											<span>确认密码：</span>
+											<input type="text">
+										</div>
+										<div class="dottedline"></div>
+										<div>
+											<span>角<i class="em"></i>色：</span>
+											<input type="text">
+										</div>
 									</div>
-									<div>
-										<span>姓<i class="em"></i>名：</span>
-										<input type="text">
+									<div class="right">
+										<div class="right-title">
+											权限
+										</div>
+										<div>
+											<table class="tb_one">
+												<tr>
+													<td>账户管理</td>
+													<td>商品管理</td>
+													<td>物流管理</td>
+												</tr>
+												<tr>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>商家资料查看</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>子账户列表查看</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>子账户安全</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>一键同步</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>商品上传</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>在售/停售商品</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>运费模板设置</span>
+														</div>
+													</td>
+												</tr>
+											</table>
+										</div>
+										<div>
+											<table class="tb_one">
+												<tr>
+													<td>交易中心</td>
+													<td>我的钱包</td>
+													<td>其他</td>
+												</tr>
+												<tr>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>我的订单</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>快捷支付单查看</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>会员管理</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>钱包余额</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>交易明细</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>公告消息</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>帮助中心</span>
+														</div>
+													</td>
+												</tr>
+											</table>
+										</div>
 									</div>
-									<div>
-										<span>账<i class="em"></i>号：</span>
-										<input type="text">
-									</div>
-									<div class="dottedline"></div>
-									<div>
-										<span>密<i class="em"></i><i class="em"></i>码：</span>
-										<input type="text">
-									</div>
-									<div>
-										<span>确认密码：</span>
-										<input type="text">
-									</div>
-									<div class="dottedline"></div>
-									<div>
-										<span>角<i class="em"></i>色：</span>
-										<input type="text">
-									</div>
+									<div class="clearfix"></div>
 								</div>
-								<div class="right">
-									<div class="right-title">
-										权限
-									</div>
-									<div>
-										<table class="tb_one">
-											<tr>
-												<td>账户管理</td>
-												<td>商品管理</td>
-												<td>物流管理</td>
-											</tr>
-											<tr>
-												<td>
-													<div>
-														<input type="checkbox">
-														<span>商家资料查看</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>子账户列表查看</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>子账户安全</span>
-													</div>
-												</td>
-												<td>
-													<div>
-														<input type="checkbox">
-														<span>一键同步</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>商品上传</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>在售/停售商品</span>
-													</div>
-												</td>
-												<td>
-													<div>
-														<input type="checkbox">
-														<span>运费模板设置</span>
-													</div>
-												</td>
-											</tr>
-										</table>
-									</div>
-									<div>
-										<table class="tb_one">
-											<tr>
-												<td>交易中心</td>
-												<td>我的钱包</td>
-												<td>其他</td>
-											</tr>
-											<tr>
-												<td>
-													<div>
-														<input type="checkbox">
-														<span>我的订单</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>快捷支付单查看</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>会员管理</span>
-													</div>
-												</td>
-												<td>
-													<div>
-														<input type="checkbox">
-														<span>钱包余额</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>交易明细</span>
-													</div>
-												</td>
-												<td>
-													<div>
-														<input type="checkbox">
-														<span>公告消息</span>
-													</div>
-													<div>
-														<input type="checkbox">
-														<span>帮助中心</span>
-													</div>
-												</td>
-											</tr>
-										</table>
-									</div>
-								</div>
-							</div>
+								<button>保存</button>
+							</form>
 						</div>
-
+						<!-- 修改 子账户 -->
+						<div class="cam-add_ac" v-if="IsShowEditDiv">
+							<div class="title">修改子账号</div>
+							<form action="">
+								<div class="body">
+									<div class="left">
+										<div>
+											<span>手机号：</span>
+											<input type="text">
+										</div>
+										<div>
+											<span>姓<i class="em"></i>名：</span>
+											<input type="text">
+										</div>
+										<div>
+											<span>账<i class="em"></i>号：</span>
+											<input type="text">
+										</div>
+										<div class="dottedline"></div>
+										<div>
+											<span>密<i class="em"></i><i class="em"></i>码：</span>
+											<input type="text">
+										</div>
+										<div>
+											<span>确认密码：</span>
+											<input type="text">
+										</div>
+										<div class="dottedline"></div>
+										<div>
+											<span>角<i class="em"></i>色：</span>
+											<input type="text">
+										</div>
+									</div>
+									<div class="right">
+										<div class="right-title">
+											权限
+										</div>
+										<div>
+											<table class="tb_one">
+												<tr>
+													<td>账户管理</td>
+													<td>商品管理</td>
+													<td>物流管理</td>
+												</tr>
+												<tr>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>商家资料查看</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>子账户列表查看</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>子账户安全</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>一键同步</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>商品上传</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>在售/停售商品</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>运费模板设置</span>
+														</div>
+													</td>
+												</tr>
+											</table>
+										</div>
+										<div>
+											<table class="tb_one">
+												<tr>
+													<td>交易中心</td>
+													<td>我的钱包</td>
+													<td>其他</td>
+												</tr>
+												<tr>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>我的订单</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>快捷支付单查看</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>会员管理</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>钱包余额</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>交易明细</span>
+														</div>
+													</td>
+													<td>
+														<div>
+															<input type="checkbox">
+															<span>公告消息</span>
+														</div>
+														<div>
+															<input type="checkbox">
+															<span>帮助中心</span>
+														</div>
+													</td>
+												</tr>
+											</table>
+										</div>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+								<button>保存</button>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -239,7 +375,10 @@
 	export default{
 		data(){
 			return{
-				IsShowModal:false
+				IsShowModal:false,
+				IsShowAddDiv:false,
+				IsShowEditDiv:false,
+				IsShowListDiv:true
 			}
 		},
 		components:{
@@ -248,6 +387,14 @@
 			FC
 		},
 		methods: {
+			_AddCA(){
+				this.IsShowListDiv = false
+				this.IsShowAddDiv = true
+			},
+			_EditCA(){
+				this.IsShowListDiv = false
+				this.IsShowEditDiv =true
+			},
 			_changeStatus(){
 				this.IsShowModal = true
 			},
