12287958144b39cf643a28a90f51a3b071fbfcd7
Liang Cheng <1101126974@qq.com>
2016-12-07 16:20:18 +0800

    code-pack

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -17,6 +17,7 @@ module.exports = merge(baseWebpackConfig, {
     loaders: utils.styleLoaders({ sourceMap: config.build.productionSourceMap, extract: true })
   },
   // devtool: config.build.productionSourceMap ? '#source-map' : false,
+  devtool: 'cheap-module-source-map',
   output: {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
