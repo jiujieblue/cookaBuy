495c0efd4a0f5c586dcd637677531692c74dd13b
Liang Cheng <1101126974@qq.com>
2016-12-15 14:00:00 +0800

    d

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -90,17 +90,17 @@ module.exports = {
   ]
 }
 
-function getEntry(globPath) {
-  var entries = {},
-    basename, tmp, pathname;
+// function getEntry(globPath) {
+//   var entries = {},
+//     basename, tmp, pathname;
 
-  glob.sync(globPath).forEach(function (entry) {
-    basename = path.basename(entry, path.extname(entry));
-    tmp = entry.split('/').splice(-3);
-    pathname = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
-    entries[pathname] = entry;
-  });
-  console.log("base-entrys:");
-  console.log(entries);
-  return entries;
-}
+//   glob.sync(globPath).forEach(function (entry) {
+//     basename = path.basename(entry, path.extname(entry));
+//     tmp = entry.split('/').splice(-3);
+//     pathname = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
+//     entries[pathname] = entry;
+//   });
+//   console.log("base-entrys:");
+//   console.log(entries);
+//   return entries;
+// }
