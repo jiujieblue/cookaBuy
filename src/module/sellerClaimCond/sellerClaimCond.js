5d56f7420c147cbf5061fd6a72ff05f8a540ed52
ybz <314763667@qq.com>
2017-01-10 17:32:20 +0800

    ybz_search_product

--- src/module/sellerClaimCond/sellerClaimCond.js
+++ src/module/sellerClaimCond/sellerClaimCond.js
@@ -5,13 +5,15 @@ import VueRouter from 'vue-router'
 import sellerClaimResults from './sellerClaimResults.vue'
 import sellerClaimChooseStore from './sellerClaimChooseStore.vue'
 import sellerClaimSubData from './sellerClaimSubData.vue'
+import sellerClaimSubDataSucc from './sellerClaimSubDataSucc.vue'
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
 const routes = [
 	{path: '/results', component: sellerClaimResults},
 	{path: '/chooseStore', component: sellerClaimChooseStore},
-	{path: '/sellerClaimSubData', component: sellerClaimSubData}
+	{path: '/subData', component: sellerClaimSubData},
+	{path: '/subDataSucc', component: sellerClaimSubDataSucc}
 ]
 
 const router = new VueRouter({
