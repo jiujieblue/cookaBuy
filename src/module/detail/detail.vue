3b8cc2f106a6612826cf8de4ac8cbbc4071d8446
Liang Cheng <1101126974@qq.com>
2017-03-07 15:13:48 +0800

    detail-less

--- src/module/detail/detail.vue
+++ src/module/detail/detail.vue
@@ -156,9 +156,7 @@
       <div class="row">
         <div class="detail-news">
           <div class="news-tit">
-            <div>
-              <div>NEW</div>
-            </div>
+            <span>NEW</span>
             <p>最新上架</p>
             <a v-on:click="_more"><span class="icon-more"></span></a>
           </div>
